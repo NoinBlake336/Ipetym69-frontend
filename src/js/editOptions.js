4e82f495a58825913d2f59efd541f118d663a578
NoinBlake <josechocobar336@gmail.com>
2023-09-22 16:21:14 -0300

    card-style

--- src/js/editOptions.js
+++ src/js/editOptions.js
@@ -3,15 +3,6 @@ export const editOptions = (idButton, typesContainer)=>{
     (typesContainer.innerHTML = `<div class="uploaded-container">
     <h2>Editar Noticias</h2>
     <div id="container-news" class="container-information">
-    <a class="card1" href="#">
-    <h3>This is option 1</h3>
-    <p class="small">Card description with lots of great facts and interesting details.</p>
-    <div class="go-corner" href="#">
-      <div class="go-arrow">
-        →
-      </div>
-    </div>
-  </a>
   <a class="card3" href="#">
     <h3>This is option 3</h3>
     <p class="small">Card description with lots of great facts and interesting details.</p>
@@ -22,6 +13,16 @@ export const editOptions = (idButton, typesContainer)=>{
       </div>
     </div>
   </a>
+  <a class="card3" href="#">
+  <h3>This is option 3</h3>
+  <p class="small">Card description with lots of great facts and interesting details.</p>
+  <div class="dimmer"></div>
+  <div class="go-corner" href="#">
+    <div class="go-arrow">
+      →
+    </div>
+  </div>
+</a>
   <a class="card3" href="#">
     <h3>This is option 3</h3>
     <p class="small">Card description with lots of great facts and interesting details.</p>
