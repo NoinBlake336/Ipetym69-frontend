305c123db6521d8a4fa976a52f52904d2e312b69
NoinBlake336 <josechocobar@ipetym69.edu.ar>
2024-04-22 04:31:30 +0200

    ApiPage

--- api/config/index.js
+++ api/config/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const config = {
     port:process.env.PORT || 3000,
-    uriDB:process.env.URLDB,
-    secretKey:process.env.SECRETKEY, 
+    uriDB:process.env.URLDB || 'mongodb+srv://pageIpetym69:ipetym692024@cluster0.xaxibxw.mongodb.net/?retryWrites=true&w=majority',
+    secretKey:process.env.SECRETKEY || "secretPageKeyJWT", 
 };
 
 module.exports = {config};
